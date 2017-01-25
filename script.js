67dc18db1e45a91d2c230f302ba852d42c1d1b6b
aderhall <muzioclavecin@gmail.com>
2017-01-25 08:59:30 -0500

    add dynamic filter hue rotate

--- script.js
+++ script.js
@@ -15,10 +15,11 @@ function doWork() {
     //document.getElementById('thething').style.color = (0xFFFFFF<<0)-document.body.style.backgroundColor;
     //document.body.style.backgroundColor ='#'+(Math.abs(Math.cos(bleh))*duh*0xFFFFFF<<0).toString(16);
     repeater = setTimeout(doWork, 10);
-    //bleh1 += 0.001;
+    bleh1 += 1;
+    bleh1 %= 360;
     //bleh2 += 0.001;
     //bleh3 += 0.001;
-    //document.body.setAttribute("style","-webkit-filter:hue-rotate(90deg)")
+    document.body.setAttribute("style","filter:hue-rotate(" + bleh1 + "deg)")
     //bleh %= 720;
     //document.getElementById("thing").innerHTML = '#'+(Math.abs(Math.cos(bleh))*duh*0xFFFFFF<<0).toString(16);
 }
